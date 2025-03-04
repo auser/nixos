d10b85b5d693436bad087674e99cd85489244258
Ari <me@ari.io>
2025-03-03 18:01:59 -0800

    Added fileSystems to config

--- systems/utm/builder/config.nix
+++ systems/utm/builder/config.nix
@@ -20,10 +20,13 @@
     device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/boot";
     fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
   };
+
   swapDevices = [
     {
       device = "/dev/disk/by-label/swap";
